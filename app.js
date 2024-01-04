56fd8387933662d75a7a27e5990e609585060464
Arthur Beck <averse.abfun@gmail.com>
2024-01-04 17:03:18 +0000

    got it working

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT ?? 3000;
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
 
-app.use(express.static('public'))
+var myLimit = 'Infinitykb';
+console.log('Using limit: ', myLimit);
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+app.use(bodyParser.json({limit: myLimit}));
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+app.all('*', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        var targetURL = "http://ftc-api.firstinspires.org/"+req.url
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': "Basic "+process.env.API_KEY} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+            }).pipe(res);
+    }
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
